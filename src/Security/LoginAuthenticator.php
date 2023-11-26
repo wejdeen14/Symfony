51e9b9302c5c437e83471304b801a4136b859914
chayma <shaymabenamor30@gmail.com>
2023-11-26 22:59:05 +0100

    first commit

--- src/Security/LoginAuthenticator.php
+++ src/Security/LoginAuthenticator.php
@@ -2,6 +2,7 @@
 
 namespace App\Security;
 
+use App\Repository\UserRepository;
 use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -9,6 +10,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\RememberMeBadge;
@@ -25,17 +27,24 @@ class LoginAuthenticator extends AbstractLoginFormAuthenticator
     public const LOGIN_ROUTE_ADMIN = 'app_login_admin';
     public const LOGIN_ROUTE_MODERATEUR = 'app_login_moderateur';
 
-    public function __construct(private UrlGeneratorInterface $urlGenerator)
+    public function __construct(private UrlGeneratorInterface $urlGenerator,private UserRepository $userRepository)
     {
     }
 
     public function authenticate(Request $request): Passport
     {
         $email = $request->request->get('email', '');
-
+        $user=$this->userRepository->findOneBy(['email'=>$email]);
+        if($user != null){
+        $userRoles = $user->getRoles();
+        $url=$this->getLoginUrl($request);
+        if (!in_array('ROLE_ADMIN', $userRoles) && str_contains($url,"admin")) {
+            throw new AuthenticationException("only admin ");
+        }
+    }
         $request->getSession()->set(Security::LAST_USERNAME, $email);
 
-        return new Passport(
+        $pas= new Passport(
             new UserBadge($email),
             new PasswordCredentials($request->request->get('password', '')),
             [
@@ -43,6 +52,7 @@ class LoginAuthenticator extends AbstractLoginFormAuthenticator
                 new RememberMeBadge(),
             ]
         );
+        return $pas;
 
     }
 
