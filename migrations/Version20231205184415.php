be9e153e3f7d4be7835b9bce700736e68648b8a8
wejden <benyaagoubwejden@gmail.com>
2023-12-06 09:14:52 +0100

    design

--- migrations/Version20231205184415.php
+++ migrations/Version20231205184415.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20231204200656 extends AbstractMigration
+final class Version20231205184415 extends AbstractMigration
 {
     public function getDescription(): string
     {
